56b39ee7614117e228b3a736c1835a3ab733c8fb
Lakshyadubey1223 <lakshya1223dubey@gmail.com>
2024-08-10 18:51:11 +0530

    Add files via upload

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'c19c6531e900b5a286e3f08856a73b39';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
